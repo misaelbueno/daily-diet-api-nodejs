c258eeac29ec4d5112ab1a8179319040f934a8d2
Misael Bueno <misa.hbueno@gmail.com>
2025-02-24 14:39:19 -0300

    add route metrics

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -109,4 +109,47 @@ export async function mealsRoutes(app: FastifyInstance) {
       return reply.status(204).send()
     }
   )
+
+  app.get('/metrics', 
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const totalMeals = await knex('meals')
+        .where({ user_id: request.user?.id })
+        .orderBy('date', 'desc')
+
+      const totalMealsOnDiet = await knex('meals')
+        .where({ user_id: request.user?.id, is_on_diet: true })
+        .count('id', { as: 'total' })
+        .first()
+
+      const totalMealsOffDiet = await knex('meals')
+        .where({ user_id: request.user?.id, is_on_diet: false })
+        .count('id', { as: 'total' })
+        .first()
+
+      const { bestOnDietSequence } = totalMeals.reduce(
+        (acc, meal) => {
+          if (meal.is_on_diet) {
+            acc.currentSequence += 1            
+          } else {
+            acc.currentSequence = 0
+          }
+
+          if (acc.currentSequence > acc.bestOnDietSequence) {
+            acc.bestOnDietSequence = acc.currentSequence
+          }
+
+          return acc
+        }, 
+        { bestOnDietSequence: 0, currentSequence: 0 },
+      )
+
+      return reply.send({
+        totalMeals: totalMeals.length,
+        totalMealsOnDiet: totalMealsOnDiet?.total,
+        totalMealsOffDiet: totalMealsOffDiet?.total,
+        bestOnDietSequence
+      })
+    }
+  )
 }
